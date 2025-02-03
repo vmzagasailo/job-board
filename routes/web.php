766f81ab47be5b11e62c86eff4615a2ad3b9d41b
vmzagasailo <vmzagasailo@gmail.com>
2025-02-03 20:18:23 +0000

    Auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\VacancyController;
 use Illuminate\Support\Facades\Route;
+
 Route::get('/', fn() => to_route('vacancies.index'));
 
 Route::get('vacancies', [VacancyController::class, 'index'])
@@ -9,3 +11,12 @@ Route::get('vacancies', [VacancyController::class, 'index'])
 
 Route::get('vacancies/{vacancy}', [VacancyController::class, 'show'])
     ->name('vacancies.show');
+
+Route::get('login', fn() => to_route('auth.create'))
+    ->name('login');
+
+Route::get('auth/create', [AuthController::class, 'create'])
+    ->name('auth.create');
+
+Route::get('auth/store', [AuthController::class, 'store'])
+    ->name('auth.store');
